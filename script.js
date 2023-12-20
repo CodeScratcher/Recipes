9dc623bf4d5ee70167698c94116a28573c1e67cb
BenjiKT <bporter2026@jpkeefehs.org>
2023-12-20 08:54:51 -0500

    did colors of xmas

--- script.js
+++ script.js
@@ -26,12 +26,12 @@ function setColor() {
         listFontColor: "black"
     };
     const christmasColorScheme = {
-        grad1: "#795c34",
-        grad2: "#4b371c",
-        grad3: "#3c280d",
-        backColor: "#b58f5c",
-        listColor: "#f5cb9c",
-        listFontColor: "black"
+        grad1: "#E91E1E",
+        grad2: "#CF1C1C",
+        grad3: "#BE1A1A",
+        backColor: "#66C739",
+        listColor: "#BB3131",
+        listFontColor: "white"
     };
     let date = new Date(Date.now());
     if (date.getMonth() == 12 && (date.getDay() >= 21 && date.getDay() <= 25)) {
